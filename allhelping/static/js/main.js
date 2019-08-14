60f288748c5b5a3bed28dfa3a052f261d64b20be
renan-dev <renanprch@gmail.com>
2019-08-14 16:36:04 -0300

    tirando overflow:hidden e melhorando a visão do código js

--- allhelping/static/js/main.js
+++ allhelping/static/js/main.js
@@ -1,33 +1,33 @@
 const btnProximo = document.querySelector('#btnNext');
 const btnNao = document.querySelector('#btnNo');
 const btnSim = document.querySelector('#btnYes');
-
 const inputNumber = document.querySelector('.inputNum');
-
 const msgErr = document.querySelector('.msgError');
-
 const num = document.querySelector('#num');
-
 const inputNumberBd = document.querySelector('#inputNumberBd');
 
-btnProximo.onclick = () => {
+function validaInput(){
     if (inputNumber.value === "" || inputNumber.value.length < 11) {
         msgErr.style = "display:block";
         inputNumber.focus();
         return false;
-    }
-    else{
-        msgErr.style = "display:none";
-        let modal = document.querySelector('#modal');
-        modal.classList.add('d-flex');
-    }
+    } 
+    msgErr.style = "display:none";
+    abrirModal();
+}
+btnProximo.addEventListener('click', validaInput);
+
+function abrirModal(){
+    let modal = document.querySelector('#modal');
+    modal.classList.add('d-flex');
     num.innerHTML = inputNumber.value;
     inputNumberBd.value = inputNumber.value;
 }
 
-btnNao.onclick = () => {
+function fechaModal(){
     let modal = document.querySelector('#modal');
     modal.classList.remove('d-flex');
     inputNumber.value = "";
     inputNumber.focus();
 }
+btnNao.addEventListener('click', fechaModal);
