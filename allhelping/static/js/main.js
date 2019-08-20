74fb03829f9e1cf127c5cf568fb15110a8a59337
attmf <anderson.maronato@etec.sp.gov.br>
2019-08-20 12:04:56 -0300

    adicionado scoll automático no chat, layout da tela de match e chat melhorados

--- allhelping/static/js/main.js
+++ allhelping/static/js/main.js
@@ -43,3 +43,31 @@ document.getElementById("ajudar").addEventListener("click", function(event){
     inputPapel = document.querySelector('#papel');
     inputPapel.value = btnAjudar.value;
 });
+
+function fMasc(objeto,mascara) {
+    obj=objeto
+    masc=mascara
+    setTimeout("fMascEx()",1)
+}
+
+function mTel(tel) {
+    tel=tel.replace(/\D/g,"")
+    tel=tel.replace(/^(\d)/,"($1")
+    tel=tel.replace(/(.{3})(\d)/,"$1)$2")
+    if(tel.length == 9) {
+        tel=tel.replace(/(.{1})$/,"-$1")
+    } else if (tel.length == 10) {
+        tel=tel.replace(/(.{2})$/,"-$1")
+    } else if (tel.length == 11) {
+        tel=tel.replace(/(.{3})$/,"-$1")
+    } else if (tel.length == 12) {
+        tel=tel.replace(/(.{4})$/,"-$1")
+    } else if (tel.length > 12) {
+        tel=tel.replace(/(.{4})$/,"-$1")
+    }
+    return tel;
+}
+
+function fMascEx() {
+    obj.value=masc(obj.value)
+}
